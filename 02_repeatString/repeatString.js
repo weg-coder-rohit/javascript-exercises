001f6ea2469f78c9d3a86bd173ca115dcd071e5b
weg-coder-rohit <roohitsingh17@gmail.com>
2024-03-05 17:03:30 +0530

    changed the repeatString function
    changed the function to take negatives value and
    return error and only add the strig if the value
    is 0 or grated than 0.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,14 @@
 const repeatString = function(str, num) {
     let returnstring = "";
-    for (let i = 0; i < num; i++) {
-        returnstring += str
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            returnstring += str
+        }
     }
+    else {
+        returnstring = "ERROR";
+    }
+
 
     return returnstring;
 };
