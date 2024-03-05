3dd8fd47bb507a98b80ef597b8aac83e5ec46ac0
weg-coder-rohit <roohitsingh17@gmail.com>
2024-03-05 16:56:04 +0530

    changed 02_repeatString.js file
    changed the function to pass the repeatString test
    this is only to pass the first test.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let returnstring = "";
+    for (let i = 0; i < num; i++) {
+        returnstring += str
+    }
 
+    return returnstring;
 };
 
 // Do not edit below this line
