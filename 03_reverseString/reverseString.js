c5716ecf4041ddb0461aab841e001fe12f50e562
weg-coder-rohit <roohitsingh17@gmail.com>
2024-03-05 21:35:30 +0530

    changed the 03_reverseString file
    have added the code to revesre a string and the
    first test case was passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let inputStringLength = inputString.length;
+    let returnString = ""
+    for (let i = inputStringLength - 1; i >= 0; i--) {
+         returnString += inputString[i];
+    }
+    return returnString;
 };
 
 // Do not edit below this line
