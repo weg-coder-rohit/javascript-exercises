a7b230ca9f0b4fbd55fe4684bcc8bcfea6b08fa9
weg-coder-rohit <roohitsingh17@gmail.com>
2024-03-08 22:20:55 +0530

    code passed the first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  return ((far - 32) * 5/9)
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return ((9/5 * cel) + 32).toFixed(1)
 };
 
 // Do not edit below this line
