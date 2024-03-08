f754c63e3bdcde5745265f5fb377e636bce364cc
weg-coder-rohit <roohitsingh17@gmail.com>
2024-03-08 21:57:36 +0530

    code written passed the first test case

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(yearToCheck) {
+    if (yearToCheck % 4 === 0){
+        if (yearToCheck % 100 === 0) {
+            if (yearToCheck % 400 === 0 ) {
+                return true
+            }
+            return false
+        }
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
