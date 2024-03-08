5bae9145d6f0a9ff0c1e74374f83adcf139a2ea4
weg-coder-rohit <roohitsingh17@gmail.com>
2024-03-08 22:44:39 +0530

    second test case passed using a parseFloat function that converts string into numbers again

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
 const convertToCelsius = function(far) {
-  return ((far - 32) * 5/9)
+  return parseFloat(((far - 32) * 5/9).toFixed(1))
   
 };
 
 const convertToFahrenheit = function(cel) {
-  return ((9/5 * cel) + 32).toFixed(1)
+  return parseFloat(((9/5 * cel) + 32).toFixed(1))
 };
 
 // Do not edit below this line
